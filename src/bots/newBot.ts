d765cfb7d53e061b0a64c666bbcb742f075b4ca5
LeoPhh <leonardo.phithak@softwire.com>
2023-06-22 10:48:22 +0100

    Added functionality that checks number of dynamites used, and another method to check the previous X moves of the other user

--- src/bots/newBot.ts
+++ src/bots/newBot.ts
@@ -3,22 +3,72 @@ import { Gamestate, BotSelection } from '../models/gamestate';
 class Bot {
 
     makeMove(gamestate: Gamestate): BotSelection {
-        const possibleMoves: BotSelection[] = ['R', 'P', 'S'];
-        const current = this.generateRandomNumber(gamestate)
 
-        this.printRound(gamestate)
+        let currentMove: BotSelection
+        const totalGamesPlayed = gamestate.rounds.length
 
-        return possibleMoves[current]
+        if (this.numberOfGamesPlayed(gamestate, 0)) {
+            currentMove = this.firstMove(gamestate)
+        } else {
+            currentMove = this.randomiseMove(gamestate, 4)
+        }
+
+
+        if (totalGamesPlayed>0) {
+            console.log(this.mostRecentRound(gamestate))
+            
+            if (this.dynamitesUsed(gamestate)==100) {
+                currentMove = this.randomiseMove(gamestate, 3)
+            }
+        }
+
+
+        if (totalGamesPlayed>50) {
+            console.log(this.detectPreviousMoves(gamestate, 4))
+        }
+        
+        
+        return currentMove
+        
     }
 
-    private generateRandomNumber(gamestate: Gamestate): number {
-        return Math.floor(Math.random()*3)
+    private firstMove(gamestate: Gamestate): BotSelection {
+        return 'P'
     }
 
-    private printRound(gamestate: Gamestate) {
-        const currentRound = gamestate.rounds[gamestate.rounds.length - 1]
-        console.log(currentRound)
+    private randomiseMove(gamestate: Gamestate, num): BotSelection {
+        const possibleMoves: BotSelection[] = ['R', 'P', 'S', 'D'];
+        const randomNum = Math.floor(Math.random()*num)
+        return possibleMoves[randomNum]
     }
+
+    private mostRecentRound(gamestate: Gamestate) {
+        return gamestate.rounds[gamestate.rounds.length - 1]
+    }
+
+    private numberOfGamesPlayed(gamestate: Gamestate, num: number): boolean {
+        return gamestate.rounds.length == num
+    }
+
+    private dynamitesUsed(gamestate: Gamestate): number {
+        let myDynamites = 0
+        for (let i =0; i<gamestate.rounds.length; i++) {
+            if (gamestate.rounds[i].p1=='D') {
+                myDynamites++
+            }
+        }
+        return myDynamites
+    }
+
+    private detectPreviousMoves(gamestate: Gamestate, moves) {
+        let previousXMoves = []
+        const totalMoves = gamestate.rounds.length
+        for (let i = totalMoves-moves; i<totalMoves; i++) {
+            previousXMoves.push(gamestate.rounds[i].p2)
+        }
+        return previousXMoves
+    }
+
 }
 
 export = new Bot();
